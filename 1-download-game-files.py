692ce86bf1573b53ccb2b4423fa8d835011fcc1a
Pranav Nutalapati <pranavnutalapati@gmail.com>
2022-07-23 16:21:29 -0400

    Reduced verbosity

--- 1-download-game-files.py
+++ 1-download-game-files.py
@@ -10,7 +10,8 @@ d = requests.get(
     "https://sieve.services.riotcdn.net/api/v1/products/lol/version-sets/PBE1?q[platform]=windows").json()
 latest = sorted((x for x in d['releases'] if x['release']['labels']['riot:artifact_type_id']['values'][0] == 'lol-game-client'),
                 key=lambda x: tuple(map(int, x['compat_version']['id'].split('+')[0].split('.'))))[-1]
+
 print(latest['download']['url'])
 
 subprocess.run(['./ManifestDownloader.exe', latest['download']['url'], '-f',
-               'DATA/FINAL/Champions/.*wad\.client$', '-t', '8', '--no-langs', '-o', 'game', '-v', '-v'])
+               'DATA/FINAL/Champions/.*wad\.client$', '-t', '8', '--no-langs', '-o', 'game', '-v'])
