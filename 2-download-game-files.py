a1195a2b52118e6fd92c2a5fc1105e90f2f669ee
Pranav Nutalapati <pranavnutalapati@gmail.com>
2022-07-23 22:51:28 -0400

    Using check_call instead of run

--- 2-download-game-files.py
+++ 2-download-game-files.py
@@ -7,5 +7,5 @@ print(os.environ)
 urllib.request.urlretrieve(
     'https://github.com/Morilli/ManifestDownloader/releases/latest/download/ManifestDownloader.exe', 'ManifestDownloader.exe')
 
-subprocess.run(['./ManifestDownloader.exe', os.environ['GAME_FILES_MANIFEST'], '-f',
-               'DATA/FINAL/Champions/.*wad\.client$', '-t', '8', '--no-langs', '-o', os.environ['GAME_FILES_PATH'], '-v'])
+subprocess.check_call(['./ManifestDownloader.exe', os.environ['GAME_FILES_MANIFEST'], '-f',
+                       'DATA/FINAL/Champions/.*wad\.client$', '-t', '8', '--no-langs', '-o', os.environ['GAME_FILES_PATH'], '-v'])
